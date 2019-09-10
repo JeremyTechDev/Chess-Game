43a65e5838d160b9e2e413660c9c4a3e02c6eebd
Jeremy Munoz Torres <jermy2918@gmail.com>
2019-09-10 11:04:07 -0400

    Created all pieces with their initial positions

--- classes.py
+++ classes.py
@@ -52,3 +52,40 @@ class king(Piece):
 class queen(Piece):
   def __init__(self, x, y, team, chessman):
     Piece.__init__(self, x, y, team, chessman) 
+
+
+#WHITES
+wp1 = pawn(6,0,"w", swp)
+wp2 = pawn(6,1,"w", swp)
+wp3 = pawn(6,2,"w", swp)
+wp4 = pawn(6,3,"w", swp)
+wp5 = pawn(6,4,"w", swp)
+wp6 = pawn(6,5,"w", swp)
+wp7 = pawn(6,6,"w", swp)
+wp8 = pawn(6,7,"w", swp)
+wr1 = rook(7,0,"w", swr)
+wr2 = rook(7,7,"w", swr)
+wk1 = knight(7,1,"w", swk)
+wk2 = knight(7,6,"w", swk)
+wb1 = bishop(7,2,"w", swb)
+wb2 = bishop(7,5,"w", swb)
+wki = king(7,3,"w", swki)
+wqu = queen(7,4,"w", swq)
+
+#BLACKS
+bp1 = pawn(1,0,"b", sbp)
+bp2 = pawn(1,1,"b", sbp)
+bp3 = pawn(1,2,"b", sbp)
+bp4 = pawn(1,3,"b", sbp)
+bp5 = pawn(1,4,"b", sbp)
+bp6 = pawn(1,5,"b", sbp)
+bp7 = pawn(1,6,"b", sbp)
+bp8 = pawn(1,7,"b", sbp)
+br1 = rook(0,0,"b", sbr)
+br2 = rook(0,7,"b", sbr)
+bk1 = knight(0,1,"b", sbk)
+bk2 = knight(0,6,"b", sbk)
+bb1 = bishop(0,2,"b", sbb)
+bb2 = bishop(0,5,"b", sbb)
+bki = king(0,3,"b", sbki)
+bqu = queen(0,4,"b", sbq)
