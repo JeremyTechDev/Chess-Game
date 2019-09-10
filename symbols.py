34341f132e9991e529965789a55d7664c5eda3fe
Jeremy Munoz Torres <jermy2918@gmail.com>
2019-09-10 10:50:31 -0400

    Created list with names of each piece

--- symbols.py
+++ symbols.py
@@ -13,3 +13,18 @@ sbr = "♜"
 sbb = "♝"
 sbk = "♞"
 sbp = "♟"
+
+chessman_names = {
+  swp: "White Pawn",
+  swr: "White Rook",
+  swk: "White Knight",
+  swb: "White Bishop",
+  swki: "White King",
+  swq: "White Queen",
+  sbp: "Black Pawn",
+  sbr: "Black Rook",
+  sbk: "Black Knight",
+  sbb: "Black Bishop",
+  sbki: "Black King",
+  sbq: "Black Queen",
+}
