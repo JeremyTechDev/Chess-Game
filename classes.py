6ebf23edd235f5e5712b6d932530b4effb14505c
Jeremy Munoz Torres <jermy2918@gmail.com>
2019-09-10 11:01:26 -0400

    Created main object (Piece)

--- classes.py
+++ classes.py
@@ -1,2 +1,17 @@
 from symbols import *
 from functions import *
+
+class Piece():
+  def __init__(self,x,y,team,chessman):
+    self.x = x
+    self.y = y
+    self.team = team
+    self.chessman = chessman
+    self.onGame = True
+
+  def kill(self):
+    self.onGame = False
+
+  def moveTo(self, x, y):
+      self.x = x
+      self.y = y
