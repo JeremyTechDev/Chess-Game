218deb76d4af335845ac2973fa7103c3f7da953d
Jeremy Munoz Torres <jermy2918@gmail.com>
2019-09-10 11:02:19 -0400

    Added important comment about the class

--- classes.py
+++ classes.py
@@ -1,6 +1,18 @@
 from symbols import *
 from functions import *
 
+"""
+PARAMETHERS:
+X -> number of the List
+Y -> index on the List Y
+team -> black or white
+chessman -> variable with the name of the symbol
+
+FUNCTIONS:
+kill -> set the piece out of the game
+moveTo -> move given piece to the position [x][y]
+"""
+
 class Piece():
   def __init__(self,x,y,team,chessman):
     self.x = x
