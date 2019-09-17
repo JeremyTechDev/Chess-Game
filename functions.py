d989ffd01a27a13aecab1ae1f71cd9c12f6a0e4d
Jeremy Munoz Torres <jermy2918@gmail.com>
2019-09-17 11:12:42 -0400

    Created getBishopMoves functions

--- functions.py
+++ functions.py
@@ -372,5 +372,68 @@ def getRookMoves(rook, x, y):
         moves.append([a, b+i+1])
     i += 1
 
-  #no need to discard Impossible Changes, they are not added to moves
+  #no need to discard Impossible Changes,
+  #they are not added to moves
   return moves
+
+#----------------------------------------------------
+#Returns ALL the moves for bishop
+def getBishopMoves(bishop, x, y):
+  moves = [] #the return item
+  a = x
+  b = y  
+
+  #stats adding the four next-moves
+  moves.append([a+1, b+1])
+  moves.append([a+1, b-1])
+  moves.append([a-1, b+1])
+  moves.append([a-1, b-1])
+
+  i = 1
+  #while pos[a,b] is on board
+  #GOING UP-RIGHT THE BOARD
+  while(not checkForPiece(a-i, b+i) and (a-i >= 0) and (b+i <= 7)):
+    moves.append([a-i, b+i])
+    
+    #checks if rook can eat a piece and add that move as possible
+    if (checkForPiece(a-i-1, b+i+1)):
+      if(getPieceAtPosition(a-i-1, b+i+1).team != bishop.team):
+        moves.append([a-i-1, b+i+1])
+        
+    i += 1
+
+
+  i = 1
+  #GOING DOWN-RIGHT THE BOARD
+  while(not checkForPiece(a+i, b+i) and (a+i <= 7) and (b+i <= 7)):
+    moves.append([a+i, b+i])
+    
+    if(checkForPiece(a+i+1, b+i+1)):
+      if (getPieceAtPosition(a+i+1, b+i+1).team != bishop.team):
+        moves.append([a+i+1, b+i+1])
+        
+    i += 1
+  
+  i = 1
+  #GOING UP-LEFT THE BOARD
+  while(not checkForPiece(a-i, b-i) and (b-i >= 0) and (a-i >= 0)):
+    moves.append([a-i, b-i])
+    
+    if(checkForPiece(a-i-1, b-i-1)):
+      if (getPieceAtPosition(a-i-1, b-i-1).team != bishop.team):
+        moves.append([a-i-1, b-i-1])
+        
+    i += 1
+
+  i = 1
+  #GOING DOWN-LEFT THE BOARD
+  while(not checkForPiece(a+i, b-i) and (b+i >= 0) and (a+i <= 7)):
+    moves.append([a+i, b-i])
+    
+    if(checkForPiece(a+i+1, b-i-1)):
+      if (getPieceAtPosition(a+i+1, b-i-1).team != bishop.team):
+        moves.append([a+i+1, b-i-1])
+        
+    i += 1
+
+  return discardImMoves(bishop, moves)
