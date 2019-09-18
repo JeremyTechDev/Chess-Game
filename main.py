c0b8b212aa68bb057022c2213e25b4d0be781fce
Jeremy Munoz Torres <jermy2918@gmail.com>
2019-09-18 10:46:05 -0400

    Created validateForPiece functions

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from classes import *
 from functions import *
 
 #Main functions of the game
+
+#return True if the choosen piece to move is able to move
+#takes in consideration if there is a piece is that pos and
+#if the piece is the same team of the player in turn
+def validateForPiece(piecePosition, team):
+  x = piecePosition[0]
+  y = piecePosition[1]
+  if not checkForPiece(x, y):
+    return False
+  else:
+    if getPieceAtPosition(x, y).team != team:
+      return False
+      
+  return True
+  
+
 """
 RUNS BY CYCLES
 >Each cycle is when both White and Black make a move.
@@ -16,8 +32,19 @@ prints the last move of the game.
 >4. The same with the next player
 """
 def startGame():
-  pass
+  printBoard(None)
+  #runs while the game is not over
+  gameOver = False
+  while not gameOver:
+    #WHITES TURN
+    #ask for the piece to move
+    print("Insert the position of the piece to move:")
+    piecePosition = toSys(input())
+    print(validateForPiece(piecePosition, "w"))
+    gameOver = True
 
+
+"""
 printBoard(None)
 movePieceTo(wk1, 3, 2)
 printBoard(None)
@@ -26,3 +53,5 @@ a = (getKnightMoves(wk1, wk1.x, wk1.y))
 printBoard(a)
 for i in a:
   print(toBoard(i[0],i[1]), end=" - ")
+"""
+startGame()
