8d56f810da3f7072545939e8c1534eefe14e51d0
Jeremy Munoz Torres <jermy2918@gmail.com>
2019-09-18 10:30:27 -0400

    Created and commented startGame function

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from classes import *
 from functions import *
 
-print_board()
+#Main functions of the game
+"""
+RUNS BY CYCLES
+>Each cycle is when both White and Black make a move.
+>Stars the game by a White Move.
+>Every move, checks if the game is finished, prints board, and
+prints the last move of the game.
+
+>For each move:
+>1. Ask the position of the piece to move
+>2. Shows all possible move for that piece
+>3. User chose one move the his or her turn is over
+>4. The same with the next player
+"""
+def startGame():
+  pass
+
+printBoard(None)
+movePieceTo(wk1, 3, 2)
+printBoard(None)
 #probitional- to print pieces' moves
-a = (getKingMoves(wki, wki.x, wki.y))
+a = (getKnightMoves(wk1, wk1.x, wk1.y))
+printBoard(a)
 for i in a:
   print(toBoard(i[0],i[1]), end=" - ")
