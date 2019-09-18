38cdbc163a6b1345af068d151883c43257178ade
Jeremy Munoz Torres <jermy2918@gmail.com>
2019-09-18 08:39:15 -0400

    Created function printIden. Prints board with move spots

--- functions.py
+++ functions.py
@@ -471,3 +471,37 @@ def getQueenMoves(queen, x, y):
       moves.append(move)
 
   return discardImMoves(queen, moves)
+
+#----------------------------------------------------------
+#Print the actual board with parenthsis to
+#indicate a possible move there
+def printIden(spots):
+  print("")
+
+  #prints each row
+  for i in range(0, 8):
+    st = ""
+    for j in range(0, 8):
+      #if piece at position [x][y], prints Piece
+      #otherwise, prints empty space '[ ]'
+      if (checkForPiece(i,j)):
+        if([i, j] in spots): #checks if move-spot in position to print ()
+          st += "(" + getSymbol(i, j)[1] + ")"
+        else: #otherwise, only prints the piece 
+          st += getSymbol(i,j)
+      else:
+        if([i, j] in spots): #checks if move-spot in position to print ()
+          st += "( )"
+        else: #otherwise, only prints []
+          st += board[i][j]
+    
+    #print num of row, then the array, and then the part 
+    #corresponding to the legend
+    print(nums[i] + st + " " + legend[i])
+
+  #print upper letters
+  print("   ", end="")
+  for i in lets:
+    print(i, end="")
+  print("")
+  print("") #separator lines
